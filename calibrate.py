8abf4d1151a53053131a79c74e1a2d49e57f2b82
Micahel5259 <63823785+Michael5259@users.noreply.github.com>
2024-11-20 13:32:59 -0600

    calibrate

--- calibrate.py
+++ calibrate.py
@@ -1,15 +1,14 @@
-import time
 import gopigo3
 from search import find
 import easygopigo3 as easy
 easy_gpg = easy.EasyGoPiGo3()
+from di_sensors.easy_line_follower import EasyLineFollower
 servo1 = easy_gpg.init_servo("SERVO1")
 servo2 = easy_gpg.init_servo("SERVO2")
 my_distance_sensor_left = easy_gpg.init_distance_sensor("AD2")
 my_distance_sensor_right = easy_gpg.init_distance_sensor("I2C")
 GPG = gopigo3.GoPiGo3()
 lineFollow = easy_gpg.init_line_follower("AD1")
+lf = easy.LineFollower("AD1")
+EasyLineFollower("AD1").calibrate("black")
 
-print(lineFollow.read())
-print(lineFollow.read("bivariate"))
-print(lineFollow.read("bivariate-str"))
