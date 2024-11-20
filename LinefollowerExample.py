660d7b2e2f9babda4df99935613633c047c229b0
Micahel5259 <63823785+Michael5259@users.noreply.github.com>
2024-11-20 11:59:30 -0600

    Line Follower

--- LinefollowerExample.py
+++ LinefollowerExample.py
@@ -9,9 +9,13 @@ my_distance_sensor_left = easy_gpg.init_distance_sensor("AD2")
 my_distance_sensor_right = easy_gpg.init_distance_sensor("I2C")
 GPG = gopigo3.GoPiGo3()
 lineFollow = easy_gpg.init_line_follower("AD1")
-
-#Checks if on white line if not go until it hits white line 
+'''
+#Checks if on white line if not go until it hits white line
 while (not "w"in lineFollow.read("bivariate-str")):
     time.sleep(0.1)
     GPG.set_motor_power(GPG.MOTOR_LEFT + GPG.MOTOR_RIGHT, 100)
 GPG.set_motor_power(GPG.MOTOR_LEFT + GPG.MOTOR_RIGHT, 0)
+'''
+print(lineFollow.read())
+print(lineFollow.read("bivariate"))
+print(lineFollow.read("bivariate-str"))
