f1e80e1c1de280c64f21cbbdcdc91b630321cd81
ThisIsNotANamepng <jjhags@gmail.com>
2024-11-20 18:26:16 -0600

    jack_1

---
+++ my_attepmt.py
@@ -0,0 +1,24 @@
+import time
+import gopigo3
+from search import find
+import easygopigo3 as easy
+easy_gpg = easy.EasyGoPiGo3()
+servo1 = easy_gpg.init_servo("SERVO1")
+servo2 = easy_gpg.init_servo("SERVO2")
+my_distance_sensor_left = easy_gpg.init_distance_sensor("I2C")
+my_distance_sensor_right = easy_gpg.init_distance_sensor("AD2")
+GPG = gopigo3.GoPiGo3()
+lineFollow = easy_gpg.init_line_follower("AD1")
+
+GPG.set_motor_power(GPG.MOTOR_LEFT + GPG.MOTOR_RIGHT, 100)
+total=0
+
+while True:
+
+    for i in lineFollow.read("bivariate"):
+        total+=i
+    if total/len(lineFollow.read("bivariate"))>0.5:
+        # Line detected
+        break
+
+GPG.set_motor_power(GPG.MOTOR_LEFT + GPG.MOTOR_RIGHT, 0)
